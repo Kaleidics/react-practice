b9bccd59196434697482f0042d2d0d2703c50e60
Eddie Chu <contact@eddiechu.me>
2019-04-09 00:01:05 -0700

    add drills folder

---
+++ react-drills/componentInteraction/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Surprise from './components/surprise';
+
+ReactDOM.render(
+  <Surprise />,
+  document.getElementById('root')
+);
