ab9ad868104e37a8f546605fd9547b569d575e96
Eddie Chu <contact@eddiechu.me>
2019-04-11 18:24:04 -0700

    update files

--- react-guess-game/src/components/Game.js
+++ react-guess-game/src/components/Game.js
@@ -1,8 +1,24 @@
-'use strict';
 
 import React from 'react';
 
 import Feedback from './Feedback';
 import GuessInput from './GuessInput';
 import GuessHistory from './GuessHistory';
+
+export default class Game extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            CurrentAnswer: '',
+            CorrectAnswer: Math.floor(Math.random() * Math.floor(100))
+        }
+    }
+
+    render(){
+        return(
+            <div><h2>The correct answer is {this.state.CorrectAnswer}</h2></div>
+        )
+       
+    }
+}
 
